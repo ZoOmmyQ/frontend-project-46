3885249e0f291e7a2fcbe4de51670b417422aa1c
ZoOmmyQ <z0om198@yandex.ru>
2024-05-05 13:18:37 +0300

    step 2 argument options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,6 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
+  .arguments('<filepath1> <filepath2>')
 program.parse();
 
