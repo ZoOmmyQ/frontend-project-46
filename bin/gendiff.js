036facb762d63c26e4a4f7a3fce428fe5d03c1d5
ZoOmmyQ <z0om198@yandex.ru>
2024-07-13 16:47:41 +0300

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
-import { program } from 'commander';
+import program from 'commander';
 import generateDiff from '../src/index.js';
 
-program 
-  .version('1.0.0', '-V, --version', 'output the version number')
+program
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format', 'stylish')
-  .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, options) => {
-    console.log(generateDiff(filepath1, filepath2, options.format));
-  });
-program.parse();
+  .option('-f, --format <type>', 'output format', 'stylish')
+  
+program.parse(process.argv);
 
