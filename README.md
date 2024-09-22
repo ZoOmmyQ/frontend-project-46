537f66a71e8630f3ff4a8f9e6cb9c3b769c7697b
ZoOmmyQ <z0om198@yandex.ru>
2024-09-22 18:20:20 +0300

    next

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/ZoOmmyQ/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/ZoOmmyQ/frontend-project-46/actions)
+
+
+
+# Difference Calculator
+### Description:
+A tool to compare two files and show the difference.
+JSON and YAML(YML) formats are supported.
+
+
+---
+#### Installation:
+
+```
+$ git clone https://github.com/ZoOmmyQ/frontend-project-46
+$ cd frontend-project-46
+$ make install
+```
+---
+### How to use:
+Enter ```gendiff -h``` to view a small help for usage
+
+[![asciicast](https://asciinema.org/a/RgpXfyEoyj4KF1VJBeRtwhsJa.svg)](https://asciinema.org/a/RgpXfyEoyj4KF1VJBeRtwhsJa)
+
+To run the program, you must specify the ```gendiff``` command and then absolute (or relative) paths to the comparison files.
+
+To specify data output type, you must add this type name with the flag -f: (```-f plain```, ```-f json```, ```-f stylish```(default))
+
+
+```
+#### Presents
+You can use ```gendiff``` to find the difference both for flat and nested YAML/JSON files.
+
+[![asciicast](https://asciinema.org/a/Ob5FXsBfvGqbMi2SzIFtSFk4C.svg)](https://asciinema.org/a/Ob5FXsBfvGqbMi2SzIFtSFk4C)
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,37 +1,37 @@
 const formatValue = (value) => {
-  if (value == null) {
-    return String(null);
-  }
-  if (typeof value === 'object') {
-    return '[complex value]';
-  }
-  if (typeof value === 'string') {
-    return `'${value}'`;
-  }
-  return value;
-};
-
-const renderPlain = (content) => {
-  const iter = (node, ancestry) => {
-    const nestedPath = [...ancestry, node.key];
-    const path = nestedPath.join('.');
-    if (node.state === 'added') {
-      return `Property '${path}' was added with value: ${formatValue(node.value)}`;
+    if (value == null) {
+      return String(null);
     }
-    if (node.state === 'deleted') {
-      return `Property '${path}' was removed`;
+    if (typeof value === 'object') {
+      return '[complex value]';
     }
-    if (node.state === 'changed') {
-      return `Property '${path}' was updated. From ${formatValue(node.oldValue)} to ${formatValue(node.newValue)}`;
+    if (typeof value === 'string') {
+      return `'${value}'`;
     }
-    if (node.state === 'unchanged') {
-      return [];
-    }
-    return node.children.flatMap((children) => iter(children, nestedPath));
+    return value;
   };
-  return content
-    .flatMap((node) => iter(node, []))
-    .join('\n');
-};
-
-export default renderPlain;
+  
+  const renderPlain = (content) => {
+    const iter = (node, ancestry) => {
+      const nestedPath = [...ancestry, node.key];
+      const path = nestedPath.join('.');
+      if (node.state === 'added') {
+        return `Property '${path}' was added with value: ${formatValue(node.value)}`;
+      }
+      if (node.state === 'deleted') {
+        return `Property '${path}' was removed`;
+      }
+      if (node.state === 'changed') {
+        return `Property '${path}' was updated. From ${formatValue(node.oldValue)} to ${formatValue(node.newValue)}`;
+      }
+      if (node.state === 'unchanged') {
+        return [];
+      }
+      return node.children.flatMap((children) => iter(children, nestedPath));
+    };
+    return content
+      .flatMap((node) => iter(node, []))
+      .join('\n');
+  };
+  
+  export default renderPlain;
